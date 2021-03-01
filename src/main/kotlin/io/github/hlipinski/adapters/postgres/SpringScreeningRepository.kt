38d26e2b1eddcfad1b3a562b08ccb54f8e0d766c
Hubert Lipiński <hubert.lipinski@gmail.com>
2021-03-01 20:52:56 +0100

    Removed unused Repository annotation

--- src/main/kotlin/io/github/hlipinski/adapters/postgres/SpringScreeningRepository.kt
+++ src/main/kotlin/io/github/hlipinski/adapters/postgres/SpringScreeningRepository.kt
@@ -4,7 +4,6 @@ import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 import java.time.Instant
 
-@Repository
 interface SpringScreeningRepository : CrudRepository<ScreeningEntity, Long> {
     fun findByScreeningTimeBetween(from: Instant, to: Instant): List<ScreeningEntity>
 }
