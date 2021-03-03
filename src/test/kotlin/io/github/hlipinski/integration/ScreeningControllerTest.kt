e0c536e09539acba3aecfd87bfd495637c24cd51
Hubert Lipiński <hubert.lipinski@gmail.com>
2021-03-03 20:44:23 +0100

    Fix mongoDB tests and split IT test files by database

--- src/test/kotlin/io/github/hlipinski/integration/ScreeningControllerTest.kt
+++ src/test/kotlin/io/github/hlipinski/integration/ScreeningControllerTest.kt
@@ -1,37 +1,14 @@
 package io.github.hlipinski.integration
 
-import io.github.hlipinski.adapters.postgres.ScreeningEntityTable
-import io.github.hlipinski.adapters.postgres.insert
-import io.github.hlipinski.adapters.postgres.toScreeningEntity
-import io.github.hlipinski.adapters.postgres.toScreeningEntityList
 import io.github.hlipinski.domain.Screening
-import io.github.hlipinski.domain.ScreeningRepository
 import io.github.hlipinski.rest.ScreeningDtoList
-import org.assertj.core.api.Assertions.assertThat
-import org.jetbrains.exposed.sql.SchemaUtils
-import org.jetbrains.exposed.sql.selectAll
-import org.jetbrains.exposed.sql.transactions.transaction
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
+import org.assertj.core.api.Assertions
 import org.springframework.boot.test.web.client.TestRestTemplate
-import org.springframework.transaction.annotation.Transactional
 import java.time.Instant
 
-@IntegrationTest
-@Transactional
-class ScreeningControllerTest
-@Autowired private constructor(private val restTemplate: TestRestTemplate) {
+open class ScreeningControllerTest(private val restTemplate: TestRestTemplate) {
 
-    @BeforeEach
-    internal fun setUp() {
-        SchemaUtils.create(ScreeningEntityTable)
-    }
-
-    @Test
-    fun `should return 1 screenings with both valid dates given`() {
-        val screening = givenScreeningInDB()
-
+    internal fun thenScreeningReturned(screening: Screening) {
         val response = restTemplate.getForObject(
             "/screenings?dateFrom=${screening.screeningTime.minusSeconds(1)}&dateTo=${
                 screening.screeningTime.plusSeconds(1)
@@ -39,22 +16,15 @@ class ScreeningControllerTest
             ScreeningDtoList::class.java
         )
 
-        assertThat(response.screenings).isNotEmpty
-        assertThat(response.screenings.size).isEqualTo(1)
+        Assertions.assertThat(response.screenings).isNotEmpty
+        Assertions.assertThat(response.screenings.size).isEqualTo(1)
     }
 
-    private fun givenScreeningInDB(
+    internal fun givenScreening(
         title: String = "ścigany",
         year: Int = 2002,
         lengthInSeconds: Int = 3600,
         screeningTime: Instant = Instant.now(),
         room: String = "redrum"
-    ): Screening {
-        val screening = Screening(1L, title, year, lengthInSeconds, screeningTime, room)
-        transaction {
-            ScreeningEntityTable.insert(screening.toScreeningEntity())
-            commit()
-        }
-        return screening
-    }
+    ): Screening = Screening(1L, title, year, lengthInSeconds, screeningTime, room)
 }
---
+++ src/test/kotlin/io/github/hlipinski/integration/mongo/MongoScreeningControllerTest.kt
@@ -0,0 +1,32 @@
+package io.github.hlipinski.integration.mongo
+
+import io.github.hlipinski.domain.Screening
+import io.github.hlipinski.domain.ScreeningRepository
+import io.github.hlipinski.integration.IntegrationTest
+import io.github.hlipinski.integration.ScreeningControllerTest
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.test.context.junit.jupiter.EnabledIf
+
+@EnabledIf(expression = "#{environment['app.main.database'] == 'mongodb'}", loadContext = true)
+@IntegrationTest
+class PostgresScreeningControllerTest @Autowired private constructor(
+    restTemplate: TestRestTemplate,
+    private val repository: ScreeningRepository) : ScreeningControllerTest(restTemplate) {
+
+    @Test
+    fun `should return 1 screenings with both valid dates given`() {
+        val screening = givenScreeningInMongoDB()
+
+        thenScreeningReturned(screening)
+    }
+
+
+
+    private fun givenScreeningInMongoDB(): Screening {
+        val screening = givenScreening()
+        repository.insert(screening)
+        return screening
+    }
+}
